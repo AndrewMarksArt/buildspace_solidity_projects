ad7d5e56511f37bb3f760ef85284869b3c25c14c
AndrewMarksArt <andrew.colin.marks@gmail.com>
2022-09-14 15:34:54 -0700

    added payable to contract and now we pay each person who waves and sends a message. updated run.js script to test and make sure it works and the contrtact is paying wavers

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,13 @@
 const main = async () => {
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1")
+    });
     await waveContract.deployed()
     console.log("Contract Addy: ", waveContract.address);
+
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance));
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
@@ -11,12 +16,15 @@ const main = async () => {
     let waveTxn = await waveContract.wave("A message!");
     await waveTxn.wait();
 
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance));
+
     const [_, randomPerson] = await hre.ethers.getSigners();
     waveTxn = await waveContract.connect(randomPerson).wave("Another message");
     await waveTxn.wait();
 
     let allWaves = await waveContract.getAllWaves();
-    console.log(allWaves);
+    // console.log(allWaves);
 };
 
 const runMain = async () => {
